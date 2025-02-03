0f5c91052e026653f18685f35407c473b4c6ea75
ezo <emersonrana@gmail.com>
2025-02-03 11:17:19 -0800

    add player list workflow

--- main.py
+++ main.py
@@ -2,3 +2,21 @@
 def changes():
     pass
 
+
+yes_choices = ["yes", "y", "ye", "yea"]
+
+add_player = True
+list_of_players = []
+
+while add_player:
+    player = input("Enter a player name: ")
+    list_of_players.append(player)
+    cont = input("Enter another player? ").lower()
+    if cont not in yes_choices:
+        print(f"List of all players: {list_of_players}")
+        correct_players = input("Is this correct? ").lower()
+        if correct_players in yes_choices:
+            add_player = False
+    else:
+        continue
+
