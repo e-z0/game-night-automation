aacaa147fcf607fe349e4367fd426ad21b11c874
ezo <emersonrana@gmail.com>
2025-02-03 17:13:13 -0800

    add comment for player workflow

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ yes_choices = ["yes", "y", "ye", "yea"]
 add_player = True
 list_of_players = []
 
+# Add players to player list workflow
+
 while add_player:
     player = input("Enter a player name: ")
     list_of_players.append(player)
